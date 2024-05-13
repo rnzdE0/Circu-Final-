a4a439d3e723fb699a98be2a6bbbd708de7a8b42
rnzdE0 <157136919+rnzdE0@users.noreply.github.com>
2024-05-14 01:06:04 +0800

    few revisions
    from meeting with lead qa and pm

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  public loginForm!: FormGroup
+  session: any
+  result: any
+  authService: any;
+  router: any;
 
+  constructor(private builder: FormBuilder,) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.builder.group({
+      user: ['', Validators.required],
+      pass: ['', Validators.required]
+    });
+  }
+
+  
+  login(){
+    console.log('Submit button clicked');
+  }
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,29 +1,31 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+import Swal from 'sweetalert2'
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
-  showPopup: boolean = false;
+  // showPopup: boolean = false;
   dropdownOpen: boolean | undefined;
   projectDropdownOpen: boolean | undefined;
 
-  togglePopup() {
-    this.showPopup = !this.showPopup;
-  }
+  // togglePopup() {
+  //   this.showPopup = !this.showPopup;
+  // }
 
-  closePopup() {
-    this.showPopup = this.showPopup;
-  }
+  // closePopup() {
+  //   this.showPopup = this.showPopup;
+  // }
 
   constructor(private router: Router) { }
 
-  redirectToLoginPage() {
-    this.router.navigate(['login']); 
-  }
+  // redirectToLoginPage() {
+  //   this.router.navigate(['login']); 
+  // }
 
   toggleDropdown() {
     this.dropdownOpen = !this.dropdownOpen;
@@ -33,5 +35,24 @@ export class MainComponent {
   toggleUserDropdown() {
     this.projectDropdownOpen = !this.projectDropdownOpen;
     this.dropdownOpen = false
+  }
+
+  logoutAlert(){
+    Swal.fire({
+      width: 400,
+      title: "Confirm Logout",
+      text: "Are you sure you want to exit Circulation?",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonColor: "#31A463",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, Log me out"
+      
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.router.navigate(['login']); 
+      }
+    });
+
   }
 }
